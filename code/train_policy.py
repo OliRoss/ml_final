469c21eceee124b51e31fb0f5e68c07612305095
Raoul Jaser Schikora <raoul.schikora@student.uibk.ac.at>
2020-02-16 19:22:20 +0100

    save after 500

--- code/train_policy.py
+++ code/train_policy.py
@@ -84,7 +84,7 @@ def reinforce(policy, step_size, render=False, num_episodes=100, gamma=0.9,log_i
         # save for all 50 episodes
         if i % SAVE_INTERVAL == 0:
             policy.save(policy.file_name + '_regular')
-        if i % 500 == 0:
+        if i % 500 == 0 and i != 0:
             policy.save(policy.file_name + '_save500')
         # Stopping criteria
         if running_reward > env.spec.reward_threshold:
